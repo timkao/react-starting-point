39b2e062b0b6e762406022681618ae9d50652459
Jerry Hsiung <jhsiung91@gmail.com>
2017-10-09 18:28:07 -0400

    created category routes

---
+++ api/categories.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const {Order, Product, LineItem, User, Category} = require('../db').models;
+
+// GET all categories includes products
+router.get('/', (req, res, next) => {
+	Category.findAll({
+		include: [Product] 
+	})
+	.then(categories =>	res.send(categories))
+	.catch(next)
+})
+
+// GET single category (by category ID) includes products
+router.get('/:id', (req, res, next) => {
+	Category.findById(req.params.id*1, {
+		include: [Product]
+	})
+	.then(category => {
+		res.send(category)
+	})
+	.catch(next)
+})
+
+// POST a new category
+router.post('/', (req, res, next) => {
+	Category.create(req.body)
+	.then(category => {
+		res.status(201)
+		res.send(category)
+	})
+	.catch(next)
+})
+
+// PUT update a category (by category ID)
+router.put('/:id', (req, res, next) => {
+	Category.findById(req.params.id*1)
+	.then(category => {
+		category.update(req.body)
+	})
+	.catch(next)
+})
+
+// DELETE a single category (by category ID)
+router.delete('/:id', (req, res, next) => {
+	Category.findById(req.params.id*1)
+	.then(category => {
+		category.destroy()
+		res.sendStatus(204)
+	})
+	.catch(next)
+})
+
+module.exports = router;
