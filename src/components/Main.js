e3c86cc5959e226f9766f2b76032a076991c9fa6
Tim Kao <pakuya.kao@gmail.com>
2017-10-09 22:56:56 -0400

    Modify Main and index

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Navbar from './Navbar'
+// import store from '../store';
+// import {thunk actions come here later} from '../store';
+import { connect } from 'react-router'
+
+export default class Main extends Component {
+
+	render(){
+		return(
+				<div>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+
+					</Switch>
+
+				</div>
+			)
+
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+// import store from './store'
+import Main from './components/Main'
+import { HashRouter as Router} from 'react-router-dom';
 
 
-ReactDOM.render(<hr />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+	<Main />
+	</Router>,
+	document.getElementById('root')
+	)
+
+//when something is in store, will look like this:
+/*
+ReactDOM.render(
+	<Provider store={store}>
+		<Main />
+	</Provider>,
+	document.getElementById('root')
+	)
+	*/
