01d4d5eb1bb49f251bf33c3aacc5580b9385078d
Tim Kao <pakuya.kao@gmail.com>
2017-10-10 12:12:25 -0400

    integrate all backend work

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 
-
 router.use('/products', require('./products'));
-// router.use('/categories', require('./categories'));
+router.use('/categories', require('./categories'));
 router.use('/users', require('./users'));
 router.use('/orders', require('./orders'));
 router.use('/lineItems', require('./lineItems'));
-// router.use('/admin', require('./admin'));
-
 
 router.use((req, res, next)=>{
 	res.status(404).send('route not found')
--- api/products.js
+++ api/products.js
@@ -3,7 +3,7 @@ const {Order, Product, LineItem, User, Category} = require('../db').models;
 
 router.get('/', (req, res, next) => {
     Product.findAll({
-        include: [Category] 
+        include: [Category]
     })
     .then(products => {
         res.send(products)
@@ -37,7 +37,7 @@ router.delete('/:productId', (req, res, next) => {
     Product.findById(req.params.productId*1)
     .then(product => {
         product.destroy()
-        res.send(product) //what to send after a delete again?
+        res.sendStatus(204)
     })
     .catch(next)
 })
