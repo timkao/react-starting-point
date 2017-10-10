dbb82add5c21496d01cbbb17056afbb6924d4f00
Tim Kao <pakuya.kao@gmail.com>
2017-10-09 22:34:48 -0400

    something wrong in test file

--- server.js
+++ server.js
@@ -6,9 +6,52 @@ const port = process.env.PORT || 3000
 const db = require('./db')
 const seed = require('./db/seed')
 const api = require('./api/index')
+const { User } = db.models
+const session = require('express-session')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'graceshopper',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// auth -- login
+app.post('/login', (req, res, next) => {
+  User.findOne({where: req.body})
+  .then( user => {
+    if (user) {
+      req.session.userId = user.id
+      res.send(user)
+    }
+    else {
+      res.sendStatus(401)
+    }
+  })
+  .catch(next)
+})
+
+// auth -- signup
+app.post('/signup', (req, res, next) => {
+  User.create(req.body)
+    .then(user => {
+      req.session.userId = user.id
+      res.send(user)
+    })
+    .catch(next)
+})
+
+// auth -- logout
+app.put('/logout', (req, res, next) => {
+  if (req.session && req.session.userId) {
+    req.session.destroy()
+    return res.send('See you again!')
+  }
+  res.send('you are not logging in!')
+})
+
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/api', api)
