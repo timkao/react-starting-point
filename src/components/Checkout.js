30b3a56adfe7b14824d309ecd6f716be3383aa8a
Jerry Hsiung <jhsiung91@gmail.com>
2017-10-17 15:41:41 -0400

    update on form

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,55 +1,85 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 
 function Checkout(props){
 	return (
-		<form className="col-lg-8">
-        <label>
-          Name: <input type="text" value=""  />
-        </label>
-        <br />
-        <label>
-          Billing Address1: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Billing Address2: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Billing City: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Billing State: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Billing Zip: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Shipping Address1: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Shipping Address2: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Shipping City: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Shipping State: <input type="text" value="" />
-        </label>
-        <br />
-        <label>
-          Shipping Zip: <input type="text" value="" />
-        </label>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+		<div>
+			<form className="col-lg-8" >
+				<div>
+        	<hr />
+        	Billing Information
+        	<hr />
+        </div>
+				<div>
+        	<label>Name:</label>
+        	<div><input name="name" type="text" value="" /></div>
+        </div>
+        <div>
+	        <label>Email:</label>
+	        <div><input name="email" type="text" value="" /></div>
+        </div>
+        <div>
+        	<label>Payment Type:</label>
+        	<div><input name="paymentType" type="radio" value="CreditCard" />Credit Card Number</div>
+        	<div><input name="paymentType" type="radio" value="PayPal" />PayPal</div>
+        </div>
+        <div>
+	        <label>Billing Address1:</label>
+	        <div><input name="billingAddress1" type="text" value="" /></div>
+        </div>       
+        <div>
+	        <label>Billing Address2:</label>
+	        <div><input name="billingAddress2" type="text" value="" /></div>
+        </div>
+        <div>
+	        <label>Billing City:</label>
+	        <div><input name="billingCity" type="text" value="" /></div>
+        </div>
+        <div>
+        	<label>Billing State:</label>
+        	<div><input name="billingState" type="text" value="" /></div>
+        </div>
+        <div>
+	        <label>Billing Zip:</label>
+	        <div><input type="text" name="billingZip" value="" /></div>
+        </div>
+        <div>
+	        <label>Billing Zip:</label>
+	        <div><input type="text" name="billingZip" value="" /></div>
+        </div>
+        
+        <div>
+        	<hr />
+        	Shipping Information
+        	<hr />
+        </div>
+        
+        <div>
+	        <label>Shipping Address1:</label>
+	        <div><input type="text" name="shippingAddress1" value="" /></div>
+        </div>
+        <div>
+       		<label>Shipping Address2:</label>
+        	<div><input type="text" name="shippingAddress2" value="" /></div>
+        </div>
+        <div>
+	        <label>Shipping City:</label>
+	        <div><input type="text" name="shippingCity" value="" /></div>
+        </div>
+        <div>
+	        <label>Shipping State:</label>
+	        <div><input type="text" name="shippingState" value="" /></div>
+        </div>
+        <div>
+	        <label>Shipping Zip:</label>
+	        <div><input type="text" name="shippingZip" value="" /></div>
+        </div>
+        <div>
+	        <button type="submit" name="submitOrder">Save</button>
+        </div>
+	    </form>
+	  </div>
 	)
 }
 
---
+++ src/store/checkout.js
@@ -0,0 +1,1 @@
+const CHECKOUT = 'CHECKOUT';
