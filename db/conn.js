c08ddd461ce5b9e2f28f6dc128346fcf5c5bb6dd
Jerry Hsiung <jhsiung91@gmail.com>
2017-10-24 01:19:06 -0400

    get rid of the sequelize deprecated error add operatorsAliases:false in conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const dataBaseUrl = process.env.DATABASE_URL || 'postgres://localhost/graceshopper'
-const conn = new Sequelize(dataBaseUrl, { logging: false });
+const conn = new Sequelize(dataBaseUrl, { logging: false, operatorsAliases: false});
 
 
 module.exports = conn;
